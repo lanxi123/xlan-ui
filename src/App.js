fd66f165ece6bca8a71fac46eb410f0bd490af2b
lanxiang <247939592@qq.com>
2020-11-08 15:59:36 +0800

    引入 antd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor() {
+    super()
+  }
+  aca() {
+    console.log(111);
+  }
+  render() {
+    return (
+      <div>
+          <Button type="primary" onClick={this.aca}>Primary Button</Button>
+      </div>
+    )
+  }
 }
 
-export default App;
+
